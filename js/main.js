e506ee1b6f85aaca98da85e8ceb4cde57afcedf3
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-06-14 14:38:44 +0200

    feat: add function random list of numbers

--- js/main.js
+++ js/main.js
@@ -28,7 +28,6 @@ start.addEventListener("click",
             container.innerHTML += newElementClassN("div", colonna + " bord" + " lock" , i);
               
         } 
-
         
         // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
         const lock = document.querySelectorAll(".lock")
@@ -39,7 +38,7 @@ start.addEventListener("click",
                     lockIesimo.classList.add("clicked");
                     console.log();
                 }
-            )
+            );
             
         }
         
@@ -49,10 +48,35 @@ start.addEventListener("click",
 );                                       
 
 
+////////////////// PARTE 2/////////////////////////////
+
+
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
 
 
 
 
+// In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+
+
+
+
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+
+
+
+
+
+
+
+// BONUS:
+
+// Quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle;
+// Quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste.
     
 
 
@@ -66,6 +90,12 @@ start.addEventListener("click",
 
 
 
+
+
+
+
+//////////////////////// FUNZIONI///////////////////////////////
+
 // creo una funzione che genra elementi e classi
 function newElementClassN(elem, classe, text){
         let newElem = `<${elem} class="${classe}">${text}</${elem}>`
@@ -73,5 +103,25 @@ function newElementClassN(elem, classe, text){
     }
 
 
+    
+// funzione sequenza numeri random
+function sequenzaNumeriRandom(min, max){
+
+    const array = [];
+
+    while(array.length < max){
+
+        // genero numero random da 1 a 16 usando la funzione
+        let newRandom = numRandom(min, max);
+        if (!array.includes(newRandom)) {
+            array.push(newRandom);
+        }
+    }    
+return array
+}
 
 
+// funzione genera numero ramndom
+function numRandom(min, max) {
+}
+    return parseInt(Math.floor(Math.random() * (1 + max - min)) + min);
