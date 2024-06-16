556d5b6f0ee19105d67c3918feb0ef1bb332e9ca
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-06-16 15:49:40 +0200

    fix: add fix to bomb selection

--- js/main.js
+++ js/main.js
@@ -9,6 +9,13 @@ const start = document.querySelector(".btn");
 const form = document.querySelector("form");
 const container = document.querySelector(".cont_grid");
 
+
+
+
+
+
+
+
 // click genera livello
 start.addEventListener("click",
     function(){
@@ -18,32 +25,58 @@ start.addEventListener("click",
         container.classList.remove("none");
         let colonna;
         if (valoreLevel===100) {
-            colonna="col10"
+            colonna="col10";
         }else if(valoreLevel===81){
-            colonna="col9"
+            colonna="col9";
         }else{
-            colonna="col7"
+            colonna="col7";
         }
-        for (let i = 1; i <= valoreLevel; i++) {
-            container.innerHTML += newElementClassN("div", colonna + " bord" + " lock" , i);
+        for ( z = 1; z <= valoreLevel; z++) {
+            
+            container.innerHTML += newElementClassN("div", colonna + " bord" + " lock" , z);
+
               
         } 
-        
+        let bomba = sequenzaNumeriRandom(1, valoreLevel,16);
+        console.log(bomba);
+       
         // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
         const lock = document.querySelectorAll(".lock")
         for (let i = 0; i < lock.length; i++) {
             const lockIesimo = lock[i];
+           
             lockIesimo.addEventListener("click",
                 function () {
-                    lockIesimo.classList.add("clicked");
-                    console.log();
+                    
+                    if (bomba.includes(parseInt(lockIesimo.textContent))) {
+                        lockIesimo.classList.add("red")
+                        
+                    }else{
+                        lockIesimo.classList.add("clicked");
+                    }
+                    
+                    
+                    // console.log(lock);
+
+                     // Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+                    // Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+                   
+                    
+
                 }
             );
             
         }
         
             
-    }   
+    }  
+    
+    
+
+
+
+
+
    
 );                                       
 
@@ -53,12 +86,14 @@ start.addEventListener("click",
 
 // Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
 // Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
-
+// let bomb = sequenzaNumeriRandom(1,16);
+// console.log(bomb);
 
 
 
 // In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
 // Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+
 
 
 
@@ -105,11 +140,11 @@ function newElementClassN(elem, classe, text){
 
     
 // funzione sequenza numeri random
-function sequenzaNumeriRandom(min, max){
+function sequenzaNumeriRandom(min, max, quantita){
 
     const array = [];
 
-    while(array.length < max){
+    while(array.length < quantita){
 
         // genero numero random da 1 a 16 usando la funzione
         let newRandom = numRandom(min, max);
@@ -117,11 +152,12 @@ function sequenzaNumeriRandom(min, max){
             array.push(newRandom);
         }
     }    
-return array
+    return array
 }
 
 
 // funzione genera numero ramndom
 function numRandom(min, max) {
-}
     return parseInt(Math.floor(Math.random() * (1 + max - min)) + min);
+}
+    
